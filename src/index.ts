662514038895f12012ce5556308144ed5b609e4b
Stefan Trpkovic <stefan.trpkovik@gmail.com>
2024-08-15 20:55:59 +0200

    end of lecture 34

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-function log(str: string) {
-  console.log(str)
-}
-
-class A {
-  greeting = 'Hello World'
-}
-
-log(new A().greeting)
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div>Hello World</div>;
+(window as any).sdfsdf
+ReactDOM.render(<App />, document.getElementById('root'))
